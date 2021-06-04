e856c16e3c41dad2595c56871714878249f942bc
maneerat.m <maneeratmaneeon@gmail.com>
2021-06-05 00:53:06 +0700

    Complete

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native'; //step 1
+import {createStackNavigator} from '@react-navigation/stack'  //step 2
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import AddUserScreen from './screens/AddUserScreen';
+import UserScreen from './screens/UserScreen';
+import UserDetailScreen from './screens/UserDetailScreen';
+
+const Stack = createStackNavigator(); //step 3
+
+//step 4
+function MyStack(){ 
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle:{
+          backgroundColor:'#0085E6'
+        },
+        headerTintColor:'#fff',
+        headerTitleStyle:{
+            fontWeight:'bold'
+        }
+      }}
+    >
+      <Stack.Screen 
+        name = "AddUserScreen"
+        component={AddUserScreen}
+        options={{title:'Add User'}}
+      />
+      <Stack.Screen 
+        name = "UserScreen"
+        component={UserScreen}
+        options={{title:'User Screen'}}
+      />
+      <Stack.Screen 
+        name = "UserDetailScreen"
+        component={UserDetailScreen}
+        options={{title:'User Detail Screen'}}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //step 5
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
